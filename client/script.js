9212a793761c624ff65ea3c107750d02ada7ecfe
Ayan De <deayan252@gmail.com>
2025-04-18 04:29:20 +0530

    "client code added"

---
+++ client/script.js
@@ -0,0 +1,26 @@
+const form = document.getElementById("form");
+const messageInput = document.getElementById("message-input");
+const roomInput = document.getElementById("room-input");
+const joinRoomButton = document.getElementById("room-button");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  const room = roomInput.value;
+
+  if (message === "") return;
+  displayMessage(message);
+
+  messageInput.value = "";
+});
+
+joinRoomButton.addEventListener("click", () => {
+  const room = roomInput.value;
+});
+
+function displayMessage(message) {
+  const div = document.createElement("div");
+  div.textContent = message;
+  div.classList.add("display-message");
+  document.getElementById("message-container").append(div);
+}
