45226c843050270c3f44a942a22206f4e3eb064d
Ayan De <deayan252@gmail.com>
2025-04-18 06:12:50 +0530

    "client and server socket setup"

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,7 @@
-import { createServer } from "http";
 import { Server } from "socket.io";
 
-const httpServer = createServer();
-const io = new Server(httpServer, {
-  // options
-});
+const io = new Server(3000, { /* options */ });
 
 io.on("connection", (socket) => {
-  // ...
-});
-
-httpServer.listen(3000,()=>{
-    console.log("Server listening at port 3000");
-    
+console.log(socket.id);
 });
